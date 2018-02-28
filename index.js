d238841efd3c4392f60bafa051f8547523caf27a
Tapan Halani <tapanhalani231@gmail.com>
2018-02-28 19:43:42 +0530

    build working, as expected.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in Los Angeles right now is: ' + currentTime.toString(),
     });
 };
 
